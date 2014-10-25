6eb9115f0116134ad8fe8ef8ad69e779a624234c
aaronce <aaron.c.eastall@gmail.com>
2014-10-25 15:48:17 +1000

    added install.sh, makes more sense i think

---
+++ install.sh
@@ -0,0 +1,5 @@
+# TODO: one time run file..
+# TODO: check for dependencies
+# TODO: download third party libs
+# TODO: run compass start project etc..
+# TODO: toggle between bourbon and compass projects on build.
