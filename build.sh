c2f2183124efca27b4a3b5aff40e5d4b3fdd9d7c
aaronce <aaron.c.eastall@gmail.com>
2014-10-25 15:35:32 +1000

    small refactor with new buildfile todo

---
+++ build.sh
@@ -0,0 +1,5 @@
+# TODO: check for dependencies
+# TODO: download third party libs
+# TODO: run compass start project etc..
+# TODO: toggle between bourbon and compass projects on build.
+# TODO: image compression and stuff, linting?
