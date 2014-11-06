1c8ef6698b78430aa8a74395cc48fd80d11b7a16
aaronce <aaron.c.eastall@gmail.com>
2014-11-06 23:19:36 +1000

    beginnings of install script

--- install.sh
+++ install.sh
@@ -1,5 +1,31 @@
+
+#!/bin/bash
+
 # TODO: one time run file..
+
 # TODO: check for dependencies
 # TODO: download third party libs
 # TODO: run compass start project etc..
 # TODO: toggle between bourbon and compass projects on build.
+
+# VARS
+GIT_IS_AVAILABLE=""
+BOURBON_PROJECT=""
+COMPASS_PROJECT=""
+
+
+# Check if git is installed.
+git --version 2>&1 >/dev/null
+GIT_IS_AVAILABLE=$?
+
+if [ $GIT_IS_AVAILABLE -eq 0 ];
+then
+	# do flag
+else
+	echo "git is not installed"
+fi
+
+
+# Ask what type of project to start
+echo "Do you want to include thoughtbot/bourbon (y / enter for no):"
+read BOURBON_PROJECT
